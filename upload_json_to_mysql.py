c282882b8c0d8b64ab66ada3ff998f4a294dd740
wyh <1872048391.com>
2021-08-21 21:22:33 +0800

    upload:strip the data

--- upload_json_to_mysql.py
+++ upload_json_to_mysql.py
@@ -1,5 +1,6 @@
 import json
 import datetime
+import string
 from sqlalchemy import create_engine
 from sqlalchemy import String, Column, Integer
 from sqlalchemy.orm import sessionmaker
@@ -23,13 +24,13 @@ for each in data:
         each['cvss_score'] = 0
         each['cvss_version'] = 0
 for each in data:
-    new_vul = VulList(cve_id=each['name'], cnnvd_id=each['other_id'], description=each['description'],
-                     vul_unit=each['vul_unit'], affected_unit=each['affected_unit'], attack_vector=each['AV'],
-                     attack_complexity=each['AC'], priviledge_required=each['PR'], 
-                     user_interaction=each['UI'], confidential=each['C'], integration=each['I'], 
-                     available=each['A'], privacy=each['P'], ecnomic=each['E'], functional=each['F'], 
-                     human_safety=each['HI'], scope=each['S'], cvss_score=each['cvss_score'], 
-                     cvss_version=each['cvss_version'], link=each['link'], date= datetime.datetime.now()
+    new_vul = VulList(cve_id=each['name'].strip(), cnnvd_id=each['other_id'].strip(), description=each['description'].strip(),
+                     vul_unit=each['vul_unit'].strip(), affected_unit=each['affected_unit'].strip(), attack_vector=each['AV'].strip(),
+                     attack_complexity=each['AC'].strip(), priviledge_required=each['PR'].strip(), 
+                     user_interaction=each['UI'].strip(), confidential=each['C'].strip(), integration=each['I'].strip(), 
+                     available=each['A'].strip(), privacy=each['P'].strip(), ecnomic=each['E'].strip(), functional=each['F'].strip(), 
+                     human_safety=each['HI'].strip(), scope=each['S'].strip(), cvss_score=each['cvss_score'], 
+                     cvss_version=each['cvss_version'], link=each['link'].strip(), date= datetime.datetime.now()
                      )
     db.add(new_vul)
 db.commit()
